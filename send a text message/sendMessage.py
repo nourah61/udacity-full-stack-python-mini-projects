181fc54fa6b0823fd150adcaa5d95056f42c18ed
sarasaj11 <sara5alhasan@gmail.com>
2018-06-07 04:47:05 +0300

    added files

--- send a text message/sendMessage.py
+++ send a text message/sendMessage.py
@@ -3,13 +3,13 @@ from twilio.rest import Client
 # Your Account SID from twilio.com/console
 account_sid = "ACa1ea2aa10eb4de0aeedf98b93e97b372"
 # Your Auth Token from twilio.com/console
-auth_token  = "your_auth_token"
+auth_token  = "5f98184a57304ef65e57826c0e480fd2"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+966590005709", #my phone number
-    from_="+17122276846", #twilio phone number
+    to="+966590005709",
+    from_="+17122276846", 
     body="Hello from Python!")
 
 print(message.sid)
